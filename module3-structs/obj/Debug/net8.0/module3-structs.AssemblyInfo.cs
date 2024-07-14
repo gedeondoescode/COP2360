4728bd51ab708da3bb27757caa39970db89603bd
Gedeon <hello@gedeondoescode.com>
2024-07-14 17:14:53 -0400

    redo of program.cs

--- module3-structs/obj/Debug/net8.0/module3-structs.AssemblyInfo.cs
+++ module3-structs/obj/Debug/net8.0/module3-structs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("module3-structs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702073c9ddc81ee1937877cab27f3ba578b3e0af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaabf451420162b6ccec59e8523ae8f575cf64d")]
 [assembly: System.Reflection.AssemblyProductAttribute("module3-structs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("module3-structs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- module3-structs/program.cs
+++ module3-structs/program.cs
@@ -1,53 +1,69 @@
-﻿// This is a usage exmaple of defining structs similar to what was provided in Structs.txt seen here: https://github.com/PBSC-Hammond/COP2360/blob/main/Module_3/Structs.txt   
+﻿// this shows uses of classes and structs as well as local methods and readonly fields
 
 using System;
 
-struct GeoCoord
-{
-    public string name;
-    public double lat, lon;
-    public string type;
-
-    // Constructor to read and reference the field values
-    public GeoCoord(string locationName, double latitude, double longitude, string locationType)
-    {
-        name = locationName;
-        lat = latitude;
-        lon = longitude;
-        type = locationType;
-    }
-};
-
-// using ref to ensure Car always lives on stack
-ref struct Car 
-{
-    public readonly int year = 2019;
-    public string make;
-    public string model;
-}
-
-// Returns a collection of locations with the correct value for each field and writes them to the console
 class Program
 {
+    void Cubes()
+    {
+        int WriteCubes(int value) => value * value * value;
+        Console.WriteLine(3);
+        Console.WriteLine(4);
+        Console.WriteLine(5);
+    }
+
     static void Main()
     {
-        // Declare a variable with our GeoCoord constructor with needed values
-        GeoCoord location = new GeoCoord("Null Island", 0.000000, 0.000000, "Landmark");
-        Console.WriteLine($"Name: {location.name}, Latitude: {location.lat}, Longitude: {location.lon}, Type: {location.type}");
+        // Octopus class
+        var octopus = new Octopus("Will");
+        octopus.getOctopiInfo();
 
-        // A struct kept on stack to be used later.
-        GeoCoord locationRef;
-        Console.WriteLine($"Name: {locationRef.name = "Palm Beach State College"}, Latitude: {locationRef.lat = 26.376310}, Longitude: {locationRef.lon = -80.099020}, Type: {locationRef.type = "College"}");
-        Console.WriteLine($"Name: {locationRef.name = "Microsoft Corporation"}, Latitude: {locationRef.lat = 47.641944}, Longitude: {locationRef.lon = -122.127222}, Type: {locationRef.type = "Business"}");
+        //using the Car struct
+        Car car = new Car("McLaren", "Senna");
+        car.getCarInfo();
 
-        // This will always return bitwise 0 or null (i.e. they're at their default values)
-        Car carDefault = default;
-        Console.WriteLine($"Year: {carDefault.year}, Make: {carDefault.make}, Model: {carDefault.model}");
+        // using Cubes function
+        WriteCubes();
+    }
+}
+
+public class Octopus
+{
+    string Name;
+    public int Age = 10;
+    public readonly int Tentacles = 8;
+
+    // Constructor for adding name
+    public Octopus(string name)
+    {
+        Name = name;
+    }
+
+    // Method for getting the octopus info
+    public void getOctopiInfo()
+    {
+        Console.WriteLine($"This octupus is named {Name} and are {Age} months old and have {Tentacles} tentacles.");
+    }
+}
+
+struct Car
+{
+    public readonly int Year = 2019;
+    public string Make;
+    public string Model;
+
+    // constructor for new car fields
+    public Car(string make, string model)
+    {
+        Make = make;
+        Model = model;
+    }
+
+    // Method for car info
+    public void getCarInfo()
+    {
+        Console.WriteLine($"The all-new {Year} {Make} {Model}!");
     }
 }
 
 
-class CarClass 
-{
-    Car carVar; //without ref for Car, carVar will live on heap. With ref, this will not compile 
-}
